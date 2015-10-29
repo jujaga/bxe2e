63c8a3186e0b78af8067afa68ab9d2d5922d3c78
Jeremy Ho <jujaga@gmail.com>
2015-10-29 16:16:15 -0700

    Reorganize AbstractLens to use Function and BiFunction interfaces
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/main/java/org/oscarehr/e2e/lens/AbstractLens.java
+++ src/main/java/org/oscarehr/e2e/lens/AbstractLens.java
@@ -1,4 +1,17 @@
 package org.oscarehr.e2e.lens;
 
-public abstract class AbstractLens<S, T> implements Lens<S, T> {
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public abstract class AbstractLens<S, T> implements Get<S, T>, Put<S, T> {
+	protected Function<S, T> get = null;
+	protected BiFunction<S, T, S> put = null;
+
+	public T get(S s) {
+		return get.apply(s);
+	}
+
+	public S put(S s, T t) {
+		return put.apply(s, t);
+	}
 }
--- src/main/java/org/oscarehr/e2e/lens/header/recordtarget/DelimiterLens.java
+++ src/main/java/org/oscarehr/e2e/lens/header/recordtarget/DelimiterLens.java
@@ -2,31 +2,26 @@ package org.oscarehr.e2e.lens.header.recordtarget;
 
 import org.marc.everest.datatypes.ADXP;
 import org.marc.everest.datatypes.AddressPartType;
-import org.oscarehr.e2e.lens.Get;
-import org.oscarehr.e2e.lens.Lens;
+import org.oscarehr.e2e.lens.AbstractLens;
 import org.oscarehr.e2e.util.EverestUtils;
 
-class DelimiterLens implements Lens<String, ADXP> {
+class DelimiterLens extends AbstractLens<String, ADXP> {
 	private static final AddressPartType addressPartType = AddressPartType.Delimiter;
 
-	private Get<String, ADXP> get = s -> {
-		ADXP addrPart = null;
-		if(!EverestUtils.isNullorEmptyorWhitespace(s)) {
-			addrPart = new ADXP(s, addressPartType);
-		}
-		return addrPart;
-	};
+	DelimiterLens() {
+		get = s -> {
+			ADXP addrPart = null;
+			if(!EverestUtils.isNullorEmptyorWhitespace(s)) {
+				addrPart = new ADXP(s, addressPartType);
+			}
+			return addrPart;
+		};
 
-	@Override
-	public ADXP get(String delimiter) {
-		return get.get(delimiter);
-	}
-
-	@Override
-	public String put(String delimiter, ADXP addrPart) {
-		if(addrPart != null && !addrPart.isNull() && addrPart.getPartType() == addressPartType) {
-			delimiter = addrPart.getValue();
-		}
-		return delimiter;
+		put = (s, t) -> {
+			if(t != null && !t.isNull() && t.getPartType() == addressPartType) {
+				s = t.getValue();
+			}
+			return s;
+		};
 	}
 }
