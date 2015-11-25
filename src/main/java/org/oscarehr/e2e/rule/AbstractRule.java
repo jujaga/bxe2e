80daf82f34b5d4cfcf329fd9f0a51fa5ab94ea4b
Jeremy Ho <jujaga@gmail.com>
2015-11-24 17:49:17 -0800

    Begin initial thoughts on TGG/Lens mashup design
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ src/main/java/org/oscarehr/e2e/rule/AbstractRule.java
@@ -0,0 +1,12 @@
+package org.oscarehr.e2e.rule;
+
+public abstract class AbstractRule<S, T> {
+	// Constructor should take in 2 things source and target: one of the two may be null
+	// Initialization of the object should detect which part is missing and build it
+	// After construction, the object is fully populated and binds the source and target
+	// Standard use should be a simple no-parameter get and put to access the objects
+	// The object itself should represent the entire TGG including the correspondence
+	
+	abstract T get();
+	abstract S put();
+}
